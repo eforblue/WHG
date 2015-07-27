27ad48e178e95acdd7d33bfd92b25c59811c3b63
Holman Gao <holman@golmansax.com>
2015-07-26 18:13:08 -0700

    Change default typo?

--- db/migrate/20150720151606_create_guesses.rb
+++ db/migrate/20150720151606_create_guesses.rb
@@ -6,12 +6,12 @@ class CreateGuesses < ActiveRecord::Migration
         t.integer :weight_male_min
         t.integer :weight_female_max
         t.integer :weight_male_max
-        t.integer :scale_female_dominant, :defalut => 1000
-        t.integer :scale_male_nondominant, :defalut => 0
-        t.integer :scale_female_even, :defalut => 1000
-        t.integer :scale_male_even, :defalut => 1000
-        t.integer :scale_female_nondominant, :defalut => 0
-        t.integer :scale_male_dominant, :defalut => 1000
+        t.integer :scale_female_dominant, :default => 1000
+        t.integer :scale_male_nondominant, :default => 0
+        t.integer :scale_female_even, :default => 1000
+        t.integer :scale_male_even, :default => 1000
+        t.integer :scale_female_nondominant, :default => 0
+        t.integer :scale_male_dominant, :default => 1000
 
       t.timestamps null: false
     end
