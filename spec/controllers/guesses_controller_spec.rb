51ca8429c6bf62f7657c655cf786cbb0de2ce510
Yang Z <yzc@Yangs-MacBook-Air.local>
2015-07-28 00:05:11 -0400

    rspec find page to test

--- spec/controllers/guesses_controller_spec.rb
+++ spec/controllers/guesses_controller_spec.rb
@@ -5,11 +5,18 @@ describe GuessesController do
 
     describe "GET #create" do
         it "should successfully provide a guess" do
-            visit "new_guess_path"
+            visit root_path
+
+            weight = rand(70..250)
+            puts "==== input weight: #{weight}"
+            fill_in "weight", :with => weight
+
         end
     end
 
     describe "GET #update" do
-        it "should successfully updat the database according to the right/wrong  guess"
+        it "should successfully updat the database according to the right/wrong  guess" do
+
+        end
     end
 end
